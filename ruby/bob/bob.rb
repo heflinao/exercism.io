d92c585431ac335b8fe0b9e17c113718e3c91073
Alex Heflin <alex@gaslight.co>
2014-01-13 14:14:00 -0500

    Working with Michael

--- ruby/bob/bob.rb
+++ ruby/bob/bob.rb
@@ -1,21 +1,18 @@
 class Bob
-  def hey(saying)  
-    while true
-      puts "Say something to Bob."
-      #end multi-line user input with "/" + Enter
-      $/ = ?\/
-      phrase = gets.chomp
-      puts conversation(phrase)
-    end
-   end
+  # def hey(saying)  
+  #   while true
+  #     puts "Say something to Bob."
+  #     #end multi-line user input with "/" + Enter
+  #     $/ = ?\/
+  #     phrase = gets.chomp
+  #     puts conversation(phrase)
+  #   end
+  #  end
   
-  def conversation(phrase)
-    #allCaps = (phrase =~ /[A-Z]{2,}+!|[A-Z]{3,}+\?|[A-Z]{4,}/)
-    #question = (phrase =~ /([\S.*+\s+]+\?+\z)/)
-    #nothing = (phrase =~ /\A\s*\z/)
-    
-    allCaps = (phrase === phrase.upcase)
-    question = (phrase == phrase.ends_with('?'))
+  def hey(phrase)
+    allCaps = (phrase =~ /[A-Z]{2,}+!|[A-Z]{3,}+\?|[A-Z]{4,}/)
+    question = (phrase =~ /([\S.*+\s+]+\?+\z)/)
+    nothing = (phrase =~ /\A\s*\z/)
 
     if allCaps
       return 'Woah, chill out!'
